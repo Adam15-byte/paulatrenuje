42f705a3420ab79658009071ab7771082aca2ded
Adam <AdamJankowski5@gmail.com>
2023-10-30 22:21:36 +0100

    add content of terms & conditions & privacy policy

---
+++ src/configs/termsConditionsConfig.ts
@@ -0,0 +1,301 @@
+export const termsConditionsConfig = [
+  {
+    title: '1.  POSTANOWIENIA WSTĘPNE',
+    content: [
+      {
+        introduction: '1. Przedmiot Regulaminu. ',
+        text: 'Niniejszy dokument określa zasady i warunki korzystania przez Klienta z Serwisu Usług Płatnych oraz stanowi regulamin świadczenia usług drogą elektroniczną w rozumieniu art. 8 ust. 1 Ustawy o świadczeniu usług drogą elektroniczną. Regulamin określa w szczególności zasady korzystania z Serwisu Usług Płatnych, składania Zamówienia, realizacji Usługi, uprawnienia Konsumenta do odstąpienia od Umowy, a także zasady składania i rozpatrywania reklamacji.',
+      },
+      {
+        introduction: '2. Usługodawca. ',
+        text: 'Podmiotem świadczącym Usługi na warunkach opisanych w niniejszym Regulaminie jest firma Adam Jankowski Venture, ul. Reymonta 4a/23, 96-500 Sochaczew, NIP 5361904234, PaulaDziubdziela.fitness@gmail.com.',
+      },
+      {
+        introduction: '3. Sposób i forma udostępnienia Regulaminu. ',
+        text: 'Aktualna treść Regulaminu jest publikowana w Serwisie Usług Płatnych pod adresem https://paulatrenuje.pl/regulamin Regulamin jest dostępny dla wszystkich, również przed zawarciem Umowy.',
+      },
+      {
+        introduction: '4. Wymóg akceptacji Regulaminów. ',
+        text: 'Umowa jest zawierana i realizowana na warunkach określonych w Regulaminie. Akceptacja Regulaminu jest warunkiem zawarcia i wykonania Umowy. Korzystanie z Serwisu wymaga przestrzegania obowiązujących w nim zasad. Z tych względów niezbędnym warunkiem zawarcia Umowy jest również akceptacja Regulaminu Serwisu.',
+      },
+      {
+        introduction:
+          '5. Wymóg potwierdzenia zapoznania się z Polityką Prywatności. ',
+        text: 'Klient zobowiązany jest do złożenia oświadczenia potwierdzającego zapoznanie się z Polityką Prywatności regulującą zasady przetwarzania danych osobowych Klienta. Oświadczenie winno zostać złożone przed złożeniem Zamówienia.',
+      },
+      {
+        introduction:
+          '6. Zgoda na przetwarzanie danych dotyczących stanu zdrowia. ',
+        text: 'Z uwagi na specyfikę Usługi, wykonanie Umowy przez Usługodawcę wymaga podania przez Klienta pełnych, rzetelnych i prawdziwych informacji o stanie zdrowia Klienta. Klient, składając Zamówienie, potwierdza, że ma tego świadomość. Po dokonaniu płatności Klient przekierowany jest do strony zawierającej ankietę personalną wymaganą do rozpoczęcia świadczenia usług. Wypełnienie i wysłanie ankiety równoznaczne jest z wyrażeniem zgody na przetwarzanie danych zdrowotnych w celu niezbędnym do prawidłowego wykonania Umowy. Usługodawca chroni dane zdrowotne w sposób szczególny, dbając w najwyższym stopniu o ich pełną poufność i wykorzystanie wyłącznie w ww. celu.',
+      },
+      {
+        introduction: '7. Uprawnienia konsumenckie. ',
+        text: 'Postanowienia niniejszego Regulaminu nie wyłączają, ani nie ograniczają jakichkolwiek uprawnień Konsumenta wynikających z przepisów powszechnie obowiązującego prawa.',
+      },
+    ],
+  },
+  {
+    title: '2.  DEFINICJE',
+    subtitle: 'Przez określenia użyte w Regulaminie należy rozumieć:',
+    content: [
+      {
+        introduction: '1. Usługodawca',
+        text: ' - firma Adam Jankowski Venture, ul. Reymonta 4a/23, 96-500 Sochaczew, NIP 5361904234, realizująca Usługi  opisane w Regulaminie.',
+      },
+      {
+        introduction: '2. Klient',
+        text: '- osoba fizyczna posiadająca pełną zdolność do czynności prawnych.',
+      },
+      {
+        introduction: '3. Konsument ',
+        text: '– osoba fizyczna dokonująca czynności prawnej.',
+      },
+      {
+        introduction: '4. Regulamin ',
+        text: '– niniejszy Regulamin Usług Płatnych określający prawa i obowiązki Klienta i Usługodawcy.',
+      },
+      {
+        introduction: '5. Serwis ',
+        text: '– serwis internetowy prowadzony przez Usługodawcę w domenie paulatrenuje.pl wraz ze wszystkimi subdomenami.',
+      },
+      {
+        introduction: '6. Umowa ',
+        text: '– umowa, której przedmiotem jest realizacja wybranej przez Klienta Usługi, zawarta na podstawie art. 750 Kodeksu cywilnego. Umowa zawarta jest w momencie otrzymania przez Klienta wiadomości e-mail potwierdzającej przyjęcie przez Usługodawcę oferty Klienta.',
+      },
+      {
+        introduction: '7. Wartość nominalna ',
+        text: '- wartość produktu lub usługi w cenach nieuwzględniających promocji, rabatów, udostępniana w Serwisie Usług Płatnych.',
+      },
+      {
+        introduction: '8. Usługa elektroniczna ',
+        text: '– usługa świadczona drogą elektroniczną przez Usługodawcę na rzecz Klienta lub innych podmiotów za pośrednictwem internetowych narzędzi komunikacji w rozumieniu Ustawy o świadczeniu usług drogą elektroniczną.',
+      },
+      {
+        introduction: '9. Zamówienie ',
+        text: '– oświadczenie woli Klienta składane Usługodawcy za pomocą Formularza zamówienia i zmierzające w swej istocie bezpośrednio do zawarcia Umowy z Usługodawcą. Złożenie Zamówienia Usługodawcy przez Klienta stanowi ofertę nabycia Usługi w rozumieniu art. 66 § 1 Kodeksu cywilnego.',
+      },
+      {
+        introduction: '10. Polityka Prywatności ',
+        text: '– regulamin zawierający informacje wymagane przez art. 13 RODO oraz regulacje odnoszące się do wykorzystywania przez Serwis plików typu cookie, dostępny pod adresem https://paulatrenuje.pl/polityka_prywatnosci',
+      },
+    ],
+  },
+  {
+    title: '3. OGÓLNE WARUNKI ŚWIADCZENIA USŁUG ELEKTRONICZNYCH.',
+    content: [
+      {
+        introduction: '1. Formularz zamówienia.',
+        text: 'Korzystanie przez Klienta z Formularza zamówienia rozpoczyna się w momencie wybrania Usługi i jej dodania do koszyka. Złożenie Zamówienia następuje niezwłocznie po wypełnieniu Formularza zamówienia i zaznaczeniu pola oznaczonego jako potwierdzenie zamówienia. W Formularzu zamówienia konieczne jest podanie następujących danych: imię i nazwisko osoby fizycznej, adres zamieszkania, e-mail, numer telefonu, zamawiana Usługa. Złożenie Zamówienia wymaga akceptacji Regulaminu.',
+      },
+      {
+        introduction: '2. Okres świadczenia usług. ',
+        text: 'Usługi świadczone są Klientowi przez czas na jaki została zawarta Umowa zgodnie z ofertą prezentowaną w Serwisie.',
+      },
+      {
+        introduction: '3. Reklamacje Usług elektronicznych. ',
+        text: 'Wszelkie reklamacje odnoszące się do Usług elektronicznych mogą być składane w formie elektronicznej na adres: PaulaDziubdziela.fitness@gmail.com lub też pisemnie na adres Adam Jankowski Venture, ul. Reymonta 4a/23, 96-500 Sochaczew. Usługodawca rozpatruje reklamacje w terminie 14 dni.',
+      },
+    ],
+  },
+  {
+    title: '4. ISTOTNE INFORMACJE PRZED ZAWARCIEM UMOWY.',
+    content: [
+      {
+        introduction: '1. Cena Usługi. ',
+        text: 'Cena Usługi wskazana na stronie Serwisu Usług Płatnych jest kwotą brutto, podana jest w nowych złotych polskich i zawiera wszystkie elementy składowe, w tym podatek VAT. Usługodawca nie dolicza żadnych dodatkowych opłat.',
+      },
+      {
+        introduction: '2. Prawa autorskie. ',
+        text: 'Treści udostępnione Klientowi. Wszelkie materiały udostępniane Klientom w związku z realizacją Usługi stanowią własność Usługodawcy i są objęte ochroną, która wynika z Ustawy z dnia 4 lutego 1994 r. o prawie autorskim i prawach pokrewnych (t.j. Dz. U. z 2018 r. poz. 1991 ze zmianami). Produkty elektroniczne dostępne na sklepie są oznaczone znakiem wodnym umożliwiającym identyfikację Konsumenta, w przypadku naruszenia Praw Autorskich.',
+      },
+      {
+        introduction: '3. Łączenie promocji. ',
+        text: 'Promocje organizowane przez Sklep nie podlegają łączeniu. Na towary objęte promocją nie obowiązują rabaty z innego tytułu, chyba że w warunkach promocji zaznaczone zostało inaczej.',
+      },
+    ],
+  },
+  {
+    title: '5. ISTOTNE INFORMACJE ZDROWOTNE DOTYCZĄCE USŁUGI.',
+    content: [
+      {
+        introduction:
+          '1. Obowiązek odbycia lekarskiej konsultacji stanu zdrowia przed zawarciem Umowy. ',
+        text: 'Klient obowiązany jest do skonsultowania swojego stanu zdrowia w okresie 6 miesięcy poprzedzających zawarcie Umowy. Klient ma prawo do zawarcia Umowy wyłącznie po odbyciu wskazanej w poprzednim zdaniu konsultacji lekarskiej, w trakcie której lekarz nie wykrył żadnych przeciwwskazań stosowania restrykcyjnej diety oraz wykonywania ćwiczeń fizycznych.',
+      },
+    ],
+  },
+  {
+    title: '6. SKŁADANIE ZAMÓWIENIA I ZAWARCIE UMOWY.',
+    content: [
+      {
+        introduction: '1. Wybór Usługi.  ',
+        text: 'Klient, za pośrednictwem Serwisu Usług Płatnych, może zamówić Usługę, która będzie aktywna przez liczbę miesięcy zgodną z wybranym Abonamentem.',
+      },
+      {
+        introduction: '2. Wybór metody płatności. ',
+        text: 'Klient ma możliwość płatności za Usługi przy wykorzystaniu systemu płatności elektronicznych oferowanych przez Przelewy24 lub PayPal.',
+      },
+      {
+        introduction: '3. Zawarcie Umowy. ',
+        text: 'Umowa zawarta jest z chwilą otrzymania przez Klienta wiadomości e-mail potwierdzającej przyjęcie przez Usługodawcę oferty Klienta.',
+      },
+    ],
+  },
+  {
+    title: '7. USTAWOWE PRAWO KONSUMENTA DO ODSTĄPIENIA OD UMOWY.',
+    content: [
+      {
+        introduction: '1. Uprawnienie do odstąpienia od Umowy. ',
+        text: 'Klient będący Konsumentem, na podstawie art. 27 Ustawy o prawach konsumenta, ma prawo do odstąpienia od umowy zawartej na odległość, bez podania przyczyny i bez ponoszenia kosztów, w terminie 14 dni licząc od zawarcia Umowy, przy czym do zachowania tego terminu wystarczające jest wysłanie oświadczenia o odstąpieniu przed jego upływem.',
+      },
+      {
+        introduction:
+          '2. Wyłączenie prawa odstąpienia od Umowy z uwagi na zgodę na wykonanie Usługi w całości. ',
+        text: 'Zgodnie z art. 38 pkt 1 Ustawy o prawach konsumenta prawo odstąpienia od Umowy zawartej na odległość nie przysługuje Klientowi w odniesieniu do umowy o świadczenie usług, jeżeli przedsiębiorca wykonał w pełni usługę za wyraźną zgodą konsumenta, który został poinformowany przed rozpoczęciem świadczenia, że po spełnieniu świadczenia przez przedsiębiorcę utraci prawo odstąpienia od umowy. Usługodawca wskazuje, że wyrażenie przez Klienta zgody na wykonanie w pełni usługi przed upływem terminu do odstąpienia od Umowy oraz wykonanie tej Usługi przez Usługodawcę, skutkuje utratą prawa odstąpienia od Umowy. Przez pełne wykonanie Usługi należy rozumieć umożliwienie klientowi pobrania produktu, którego zakupu dokonał. Akceptacja regulaminu przed dokonaniem płatności równoznaczna jest z akceptacją przez Klienta utraty prawa do odstąpienia od umowy zgodnie z zapisami art. 38 Ustawy o prawach konsumenta.',
+      },
+      {
+        introduction:
+          '3. Wyłączenie prawa odstąpienia od Umowy z uwagi na zgodę na rozpoczęcie wykonania Usługi. ',
+        text: 'Zgodnie z art. 38 pkt 13 Ustawy o prawach konsumenta prawo odstąpienia od Umowy zawartej na odległość nie przysługuje Klientowi w odniesieniu do umowy o dostarczanie treści cyfrowych, które nie są zapisane na nośniku materialnym, jeżeli spełnianie świadczenia rozpoczęło się za wyraźną zgodą konsumenta przed upływem terminu do odstąpienia od umowy i po poinformowaniu go przez przedsiębiorcę o utracie prawa odstąpienia od umowy. Usługodawca wskazuje, że wyrażenie przez Klienta zgody na rozpoczęcie spełnienia świadczenia przed upływem terminu do odstąpienia od Umowy skutkuje utratą prawa odstąpienia od Umowy.',
+      },
+      {
+        introduction: '4. Sposób odstąpienia od Umowy.  ',
+        text: 'Oświadczenie o odstąpieniu od Umowy Konsument może złożyć  poprzez wysłanie wiadomości elektronicznej na adres PaulaDziubdziela.fitness@gmail.com w której wyrazi chęć odstąpienia od zawartej umowy wraz z numerem złożonego zamówienia.',
+      },
+      {
+        introduction: '5. Zwrot płatności. ',
+        text: 'Usługodawca, niezwłocznie lecz nie później niż w terminie 14 dni licząc od dnia otrzymania oświadczenia o odstąpieniu od Umowy złożonego przez Konsumenta, zwróci Konsumentowi wszystkie dokonane przez niego płatności, z zastrzeżeniem wyjątków wynikających z Ustawy o prawach konsumenta.',
+      },
+      {
+        introduction: '6. Sposób zwrotu płatności. ',
+        text: 'Usługodawca dokonuje zwrotu zapłaty dokonanej przez Konsumenta takim samym sposobem płatności, jakiego użył Konsument.',
+      },
+    ],
+  },
+  {
+    title: '8. ODPOWIEDZIALNOŚĆ USŁUGODAWCY I TRYB REKLAMACYJNY.',
+    content: [
+      {
+        introduction:
+          '1. Odpowiedzialność Usługodawcy związana z wykonaniem Umowy. ',
+        text: 'Usługodawca ponosi odpowiedzialność za należyte wykonanie Umowy na zasadach ogólnych, z zastrzeżeniem odrębności wynikających z Regulaminu.',
+      },
+      {
+        introduction:
+          '2. Charakter prawny informacji udzielanych przez Usługodawcę. ',
+        text: 'Usługodawca udziela jedynie zaleceń i wskazówek dotyczących żywienia i treningu oraz suplementacji, mających na celu poprawne wykonywanie ćwiczeń i odpowiednie odżywianie, i nie jest on odpowiedzialny za ewentualne występowanie efektów ubocznych z wykonywania i stosowania się do rzeczonych zaleceń. Udzielane zalecenia i wskazówki pod żadnym pozorem nie mogą zastąpić indywidualnej porady farmaceutycznej, lekarskiej, psychologicznej, dietetyka klinicznego czy stacjonarnego.',
+      },
+      {
+        introduction:
+          '3. Wpływ należytego wykonania Umowy na oczekiwane efekty. ',
+        text: 'Klient, zawierając Umowę i akceptując Regulamin, potwierdza, że ma świadomość, iż na poprawę stanu zdrowia wpływ ma szereg czynników zewnętrznych, niezależnych od Usługodawcy, w szczególności choć nie wyłącznie, nieodpowiednie wykonywanie ćwiczeń lub niestosowanie się do wszystkich zaleceń planów żywieniowych.',
+      },
+      {
+        introduction: '4. Forma reklamacji. ',
+        text: 'Reklamacje z tytułu odpowiedzialności Usługodawcy za należyte wykonanie Umowy, Konsument może złożyć w formie elektronicznej poprzez wysłanie e-maila na adres PaulaDziubdziela.fitness@gmail.com lub też pisemnie na adres Adam Jankowski Venture, ul. Reymonta 4a/23, 96-500 Sochaczew. 6. Treść zgłoszenia reklamacyjnego. Usługodawca rekomenduje, aby zgłoszenie reklamacyjne zawierało w szczególności następujące informacje:',
+      },
+      {
+        introduction: '',
+        text: 'a) dane kontaktowe Konsumenta składającego reklamację (imię, nazwisko, email),',
+      },
+      {
+        introduction: '',
+        text: 'b) reklamowana Usługa oraz numer Zamówienia,',
+      },
+      {
+        introduction: '',
+        text: 'c) szczegółowy opis nienależytego wykonania Umowy,',
+      },
+      {
+        introduction: '',
+        text: 'd) reklamacyjne żądanie Klienta. Zawartość reklamacji, o której mowa powyżej, ma charakter zalecenia i nie wpływa na skuteczność reklamacji złożonych z jej pominięciem.',
+      },
+      {
+        introduction: '5. Termin rozpatrzenia reklamacji. ',
+        text: 'Wszelkie reklamacje rozpatrywane są w terminie 14 dni od daty ich otrzymania przez Usługodawcę.',
+      },
+    ],
+  },
+  {
+    title: '9. DANE OSOBOWE.',
+    content: [
+      {
+        introduction: '1. Administrator danych. ',
+        text: 'Administratorem danych osobowych Klientów jest Usługodawca.',
+      },
+      {
+        introduction: '2. Cel przetwarzania danych. ',
+        text: 'Dane osobowe przetwarzane są w celu: złożenia Zamówienia, wykonania Umowy, rozpatrzenia reklamacji, dochodzenia i obrony przed ewentualnymi roszczeniami,  obsługi złożonego zapytania, komunikacji z Klientem, przedstawienia oferty sprzedażowej.',
+      },
+      {
+        introduction: '3. Uprawnienia Klienta. ',
+        text: 'Klient ma prawo dostępu do treści swoich danych, ich poprawiania, przenoszenia, złożenia sprzeciwu w przypadku świadczenia usług marketingowych na podstawie uzasadnionego interesu prawnego administratora, wycofania zgody, o ile podstawą przetwarzania danych jest zgoda, ograniczenia przetwarzania. Podanie danych jest dobrowolne jednak konieczne do realizacji poszczególnych celów.',
+      },
+    ],
+  },
+];
+
+export const privacyPolicy = [
+  {
+    title: '1. DEFINICJE.',
+    subtitle:
+      'Na potrzeby niniejszej Polityki wykorzystywane są poniższe pojęcia:',
+    content: [
+      {
+        introduction: '1. Administrator ',
+        text: '– firma Adam Jankowski Venture, ul. Reymonta 4a/23, 96-500 Sochaczew, NIP 5361904234, e-mail: PaulaDziubdziela.fitness@gmail.com, czyli podmiot, który zgodnie z RODO decyduje o sposobach i celach przetwarzania Twoich danych osobowych.',
+      },
+      {
+        introduction: '2. Dane osobowe ',
+        text: '– wszystkie informacje o osobie fizycznej zidentyfikowanej lub możliwej do zidentyfikowania poprzez jeden bądź kilka szczególnych czynników określających fizyczną, fizjologiczną, genetyczną, psychiczną, ekonomiczną, kulturową lub społeczną tożsamość, w tym IP urządzenia, dane o lokalizacji, identyfikator internetowy oraz informacje gromadzone za pośrednictwem plików cookie oraz innej podobnej technologii.',
+      },
+      {
+        introduction: '3. Polityka ',
+        text: '– niniejsza Polityka prywatności.',
+      },
+      {
+        introduction: '4. RODO ',
+        text: '– Rozporządzenie Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE.',
+      },
+      {
+        introduction: '5. Serwis ',
+        text: '– serwis internetowy prowadzony przez nas pod adresem www.paulatrenuje.pl wraz ze wszystkimi subdomenami, w szczególności Serwisem Usług Płatnych.',
+      },
+      {
+        introduction: '6. Użytkownik, Klient ',
+        text: '– każda osoba fizyczna odwiedzająca Serwis lub, w zależności od kontekstu i sytuacji, zamierzająca korzystać lub korzystająca z usług, produktów oferowanych w Serwisie.',
+      },
+    ],
+  },
+  {
+    title: '2. CELE PRZETWARZANIA DANYCH OSOBOWYCH.',
+    subtitle:
+      'Zbieramy, przetwarzamy i wykorzystujemy Twoje dane osobowe w następujących celach:',
+    content: [
+      {
+        introduction: '',
+        text: '1. Zawarcia i wykonania umowy w zakresie świadczenia innych usług drogą elektroniczną.',
+      },
+      {
+        introduction: '',
+        text: '2. Obsługi potencjalnego klienta przed zawarciem umowy, w procesie zawierania umowy oraz po zawarciu umowy – w formie telefonicznej oraz elektronicznej.',
+      },
+      {
+        introduction: '',
+        text: '3. Rozpatrzenia reklamacji, obrony przed potencjalnymi roszczeniami, dochodzenia potencjalnych roszczeń.',
+      },
+    ],
+  },
+  {
+    title: '3. PODSTAWY PRAWNE PRZETWARZANIA DANYCH.',
+    subtitle:
+      'Twoje dane osobowe mogą być przetwarzane w oparciu o Art. 6 oraz Art. 9 RODO stanowiące podstawę prawną do czynności związanych z przetwarzaniem danych.',
+    content: [],
+  },
+  {
+    title: '4. PRAWO I OBOWIĄZEK PODANIA DANYCH.',
+    subtitle:
+      'Podanie danych osobowych jest wymagane do podjęcia umowy i przetworzenia zamówienia. Odmówienie przetwarzania danych wyrażone przez niepodanie wszystkich wymaganych danych, równoznaczne jest z uniemożliwieniem realizacji umowy.',
+    content: [],
+  },
+];
