da5eaaf8d285ef694162d9253722bc1a16b01f87
Adam <AdamJankowski5@gmail.com>
2024-01-01 16:54:56 +0100

    remove animations from listItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import useWindowDimensions from '@/hooks/useWindowDimension';
-import { motion } from 'framer-motion';
 import { Check } from 'lucide-react';
 import { FC } from 'react';
 
@@ -11,46 +9,12 @@ interface ListItemProps {
 }
 
 const ListItem: FC<ListItemProps> = ({ text, index }) => {
-  const { width: screenWidth } = useWindowDimensions();
-  const isScreenSmall = screenWidth < 700;
-  if (isScreenSmall) {
-    return (
-      <div className="flex items-center gap-4 group">
-        <div className="text-white bg-gray group-hover:bg-orange transition-all p-1 rounded-full flex items-center justify-center">
-          <Check size={12} />
-        </div>
-        <div className="tracking-wide leading-6">{text}</div>
-      </div>
-    );
-  }
   return (
     <div className="flex items-center gap-4 group">
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: false }}
-        transition={{ duration: 0.2, delay: index * 0.1 }}
-        variants={{
-          visible: { opacity: 1, rotate: 0 },
-          hidden: { opacity: 0, rotate: 45 },
-        }}
-        className="text-white bg-gray group-hover:bg-orange transition-all p-1 rounded-full flex items-center justify-center"
-      >
+      <div className="text-white bg-gray group-hover:bg-orange transition-all p-1 rounded-full flex items-center justify-center">
         <Check size={12} />
-      </motion.div>
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: false }}
-        transition={{ duration: 0.2, delay: index * 0.1 }}
-        variants={{
-          visible: { opacity: 1, x: 0 },
-          hidden: { opacity: 0, x: 50 },
-        }}
-        className="tracking-wide leading-6"
-      >
-        {text}
-      </motion.div>
+      </div>
+      <div className="tracking-wide leading-6">{text}</div>
     </div>
   );
 };
