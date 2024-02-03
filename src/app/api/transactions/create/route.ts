66a18baab995b1322e3d752944fae458f780263b
Adam <AdamJankowski5@gmail.com>
2024-02-03 12:26:46 +0100

    debug transaction creator

--- src/app/api/transactions/create/route.ts
+++ src/app/api/transactions/create/route.ts
@@ -6,16 +6,18 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
     const validatedData = TransactionValidator.parse(body);
+    console.log('validatedData', validatedData);
     await db.transaction.create({
       data: {
         ...validatedData,
       },
     });
+    console.log('created in database');
     return new Response('Ok');
   } catch (e) {
     if (e instanceof z.ZodError) {
       return new Response(e.message, { status: 422 });
     }
-    return new Response('Could not create a promo code', { status: 500 });
+    return new Response('Could not createa a new transaction', { status: 500 });
   }
 }
--- src/app/koszyk/zamowienie/page.tsx
+++ src/app/koszyk/zamowienie/page.tsx
@@ -129,6 +129,7 @@ const Page: FC = () => {
 
   const onSubmit: SubmitHandler<IFormInput> = async (formData) => {
     const sessionId = await generateTransactionId();
+    console.log('sessionId', sessionId);
     //register transaction in p24
     const order: Order = {
       sessionId: sessionId,
@@ -146,10 +147,12 @@ const Page: FC = () => {
       regulationAccept: p24Rules,
       waitForResult: true,
     };
+    console.log('order', order);
     const result = await axios.post(
       '/api/transactions/register-transaction-p24',
       order
     );
+    console.log('result', result);
     // create transaction in database
     const transactionBody: ITransactionValidator = {
       id: sessionId,
@@ -163,6 +166,7 @@ const Page: FC = () => {
       createdAt: new Date().toISOString(),
     };
     await axios.post('/api/transactions/create', transactionBody);
+    console.log('after create transaction');
     router.push(result.data.link);
   };
 
